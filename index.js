38b9c3f657e379dd83a946f3354e537297ae8ff5
ShivBhanKushWaha <shivbhankushwaha862003672001@gmail.com>
2025-03-31 20:28:59 +0530

    Done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import userRoute from "./routes/user.route.js";
 import messageRouter from "./routes/message.route.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./SocketIO/server.js";
-import path from "path";
+// import path from "path";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const URI = process.env.MongoDbURI;
@@ -28,13 +28,13 @@ mongoose
   .catch((err) => console.log("Error while connecting to the database", err));
 
 // deployment code
-if (process.env.NODE_ENV === "production") {
-  const dirPath = path.resolve();
-  app.use(express.static("./Frontend/dist"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(dirPath, "./Frontend/dist", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   const dirPath = path.resolve();
+//   app.use(express.static("./Frontend/dist"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(dirPath, "./Frontend/dist", "index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
